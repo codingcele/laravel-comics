859c5c9a0829ad761f5d9ad3334af240c3af4c6c
codingcele <marcocelegato9@gmail.com>
2023-02-01 13:10:07 +0100

    first commit

---
+++ resources/views/pages/comics.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main-layout')
+
+@section('content')
+    <div class="container-fluid d-flex justify-content-space-between align-items-center my_header">
+        <div class="col-3 d-flex justify-content-center h-75">
+            <img class="pointer" src="{{ Vite::asset("resources/img/dc-logo.png") }}" alt="">
+        </div>
+        <div class="col-6 d-flex justify-content-around">
+            <div class="my_fs_10px pointer">CHARACTERS</div>
+            <div class="my_fs_10px pointer">COMICS</div>
+            <div class="my_fs_10px pointer">MOVIES</div>
+            <div class="my_fs_10px pointer">TV</div>
+            <div class="my_fs_10px pointer">GAMES</div>
+            <div class="my_fs_10px pointer">COLLECTIBLES</div>
+            <div class="my_fs_10px pointer">VIDEOS</div>
+            <div class="my_fs_10px pointer">FANS</div>
+            <div class="my_fs_10px pointer">NEWS</div>
+            <div class="my_fs_10px pointer">SHOP</div>
+        </div>
+        <div class="col-3 my_fs_13px my_search pointer">
+            <div class="d-flex justify-content-end">Search <i class="fa-solid fa-magnifying-glass d-flex align-items-center magni"></i></div>
+            <div class="blueLine"></div>
+        </div>
+    </div>
+    <div class = "container-fluid my_jumbotron">
+        <div class="col-3 currentSeries">CURRENT SERIES</div>
+    </div>
+@endsection
