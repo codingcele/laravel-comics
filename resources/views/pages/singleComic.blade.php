03a3054aae04fc2cd04c96e507b1f0994a2ea73d
codingcele <marcocelegato9@gmail.com>
2023-02-06 11:57:51 +0100

    immagine aggiunta a singleComic

--- resources/views/pages/singleComic.blade.php
+++ resources/views/pages/singleComic.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>Ciao Comic</h1>
+    <div class="blueDiv">
+        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    </div>
 @endsection
